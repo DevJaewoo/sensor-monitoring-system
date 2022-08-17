cc2cc37515868c88d6567a071950b4b4d537f80c
Jaewoo Ahn <DevJaewoo@gmail.com>
2022-08-17 20:38:38 +0900

    Init Server

---
+++ Server/src/main/java/com/example/sensormonitoringserver/SensorMonitoringServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sensormonitoringserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorMonitoringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorMonitoringServerApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/com/example/sensormonitoringserver/SensorMonitoringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sensormonitoringserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorMonitoringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
