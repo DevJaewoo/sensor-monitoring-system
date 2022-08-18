23e3ab0891d08050f051c288dc93286ee6e28059
Jaewoo Ahn <DevJaewoo@gmail.com>
2022-08-18 15:34:01 +0900

    Init React

--- Server/src/main/java/com/example/sensormonitoringserver/controller/SensorController.java
+++ Server/src/main/java/com/example/sensormonitoringserver/controller/SensorController.java
@@ -3,9 +3,15 @@ package com.example.sensormonitoringserver.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
 public class SensorController {
 
+    @ResponseBody
+    @GetMapping("/api/hello")
+    public String test() {
+        return "Hello, world!";
+    }
 }
