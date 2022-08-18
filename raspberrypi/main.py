a40cb030f89c85eb94a5e521705e173fb4960c4f
DevJaewoo <DevJaewoo@gmail.com>
2022-08-18 11:52:44 +0900

    Request from Raspberry Pi

--- raspberrypi/main.py
+++ raspberrypi/main.py
@@ -1,7 +1,14 @@
+import requests
+
 import smbus
 from time import sleep
 from CCS811 import CCS811
 from MPU6050 import MPU6050
+
+SERVER_IP = '192.168.166.26'
+SERVER_PORT = 8080
+CLIENT_ID = 1
+REQUEST_URL = 'http://{0}:{1}/api/{2}/sensor'.format(SERVER_IP, SERVER_PORT, CLIENT_ID)
 
 i2c = smbus.SMBus(1)
 
@@ -11,12 +18,32 @@ sensor = MPU6050(i2c)
 sleep(0.1)
 
 while True:
-	try:
-		[eco2, tvoc] = ccs811.readData()
-		print('eCO2: {} TVOC: {}'.format(eco2, tvoc))
-		print(sensor.get_accel_data())
-		print(sensor.get_temp_data())
-	except Exception as e:
-		print('Except: {}'.format(e))
 
 	sleep(1)
+
+	try:
+		[eco2, tvoc] = ccs811.readData()
+		accel = sensor.get_accel_data()
+		temp = sensor.get_temp_data()
+
+		if (eco2 == 0 or accel['x'] == 0):
+			print('Trash value')
+			continue
+
+		print('eCO2: {} TVOC: {} Temp: {}\nAccel: {}'.format(eco2, tvoc, temp, accel))
+
+		json = {
+			'eco2': eco2,
+			'tvoc': tvoc,
+			'temp': round(temp, 2),
+			'accel': {
+				'x': round(accel['x'], 2),
+				'y': round(accel['y'], 2),
+				'z': round(accel['z'], 2)
+			}
+		}
+
+		response = requests.post(url = REQUEST_URL, json = json)
+
+	except Exception as e:
+		print('Except: {}'.format(e))
