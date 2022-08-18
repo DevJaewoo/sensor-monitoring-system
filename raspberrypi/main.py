57a9c28c72aa616388f3540faf957a9c074e3ae4
DevJaewoo <DevJaewoo@gmail.com>
2022-08-19 01:27:03 +0900

    Re-Init after accelerometer sensor sleeps

--- raspberrypi/main.py
+++ raspberrypi/main.py
@@ -5,12 +5,14 @@ from time import sleep
 from CCS811 import CCS811
 from MPU6050 import MPU6050
 
-SERVER_IP = '192.168.166.26'
+import traceback
+
+SERVER_IP = '192.168.166.112'
 SERVER_PORT = 8080
 CLIENT_ID = 1
 REQUEST_URL = 'http://{0}:{1}/api/{2}/sensor'.format(SERVER_IP, SERVER_PORT, CLIENT_ID)
 
-SENSOR_INTERVAL = 5 # sec
+SENSOR_INTERVAL = 1 # sec
 
 i2c = smbus.SMBus(1)
 
@@ -26,13 +28,15 @@ while True:
 	try:
 		[eco2, tvoc] = ccs811.readData()
 		accel = sensor.get_accel_data()
+		sleep(0.1)
 		temp = sensor.get_temp_data()
 
+		print('eCO2: {} TVOC: {} Temp: {}\nAccel: {}'.format(eco2, tvoc, temp, accel))
+
 		if (eco2 == 0 or accel['x'] == 0):
+			i2c.write_i2c_block_data(0x68, 0x6B, [0x20])
 			print('Trash value')
 			continue
-
-		print('eCO2: {} TVOC: {} Temp: {}\nAccel: {}'.format(eco2, tvoc, temp, accel))
 
 		json = {
 			'eco2': eco2,
@@ -48,4 +52,5 @@ while True:
 		response = requests.post(url = REQUEST_URL, json = json)
 
 	except Exception as e:
+		traceback.print_exc()
 		print('Except: {}'.format(e))
