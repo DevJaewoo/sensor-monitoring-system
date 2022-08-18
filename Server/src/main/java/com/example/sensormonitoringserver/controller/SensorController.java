3df93103bd691fe43394e819e60c76a26e472169
Jaewoo Ahn <DevJaewoo@gmail.com>
2022-08-18 14:38:53 +0900

    Moved API Controller

--- Server/src/main/java/com/example/sensormonitoringserver/controller/SensorController.java
+++ Server/src/main/java/com/example/sensormonitoringserver/controller/SensorController.java
@@ -1,32 +1,11 @@
 package com.example.sensormonitoringserver.controller;
 
-import com.example.sensormonitoringserver.dto.SensorCreateRequestDto;
-import com.example.sensormonitoringserver.dto.SensorDto;
-import com.example.sensormonitoringserver.dto.SensorSearch;
-import com.example.sensormonitoringserver.service.SensorService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.validation.Valid;
-import java.util.List;
-
-@RequestMapping("/api")
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class SensorController {
 
-    private final SensorService sensorService;
-
-    @PostMapping("/{id}/sensor")
-    public ResponseEntity<?> createSensorData(@PathVariable Long id, @Valid @RequestBody SensorCreateRequestDto sensorCreateRequestDTO) {
-        sensorService.uploadSensorData(id, sensorCreateRequestDTO);
-        return ResponseEntity.ok("");
-    }
-
-    @GetMapping("/{id}/sensor")
-    public ResponseEntity<?> readSensorData(@PathVariable Long id, SensorSearch sensorSearch) {
-        List<SensorDto> result = sensorService.searchSensorData(id, sensorSearch);
-        return ResponseEntity.ok(new SensorDto.ResponseList(result));
-    }
 }
